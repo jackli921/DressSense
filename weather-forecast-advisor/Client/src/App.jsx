d8f3a30ac8a65788e49a1cb99d62d2fc2c043637
Jack <li.jack0707@gmail.com>
2023-04-28 11:15:48 -0400

    chore: create prompt for chatgpt api

--- weather-forecast-advisor/Client/src/App.jsx
+++ weather-forecast-advisor/Client/src/App.jsx
@@ -1,35 +1,46 @@
-
 import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
 
-
-async function getApiKey() {
-  const response = await fetch("http://localhost:3000/api_key");
-  const data = await response.json();
-  const apiKey = data.apiKey;
-  // use apiKey in your weather API request
-  getWeatherData(apiKey)
-}
-
-async function getWeatherData(apiKey) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&APPID=${apiKey}`;
-  try {
-        const response = await fetch(url);
-        const data = await response.json()
-        console.log(data)
+  async function getApiKey() {
+    const response = await fetch("http://localhost:3000/api_key");
+    const data = await response.json();
+    const apiKey = data.apiKey;
+    // use apiKey in your weather API request
+    getWeatherData(apiKey);
   }
-    catch (error) {
-    console.log(error, "error fetching the api key");
+
+  async function getWeatherData(apiKey) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&APPID=${apiKey}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      makePrompt(data);
+    } catch (error) {
+      console.log(error, "error fetching the api key");
+    }
   }
-}
+
+  // create a function to concatenate human prompt with weather data
+  function makePrompt(data) {
+    const sentence =
+      "Could you give me some suggestions for what to wear today given the following weather data in my city, which is also inside the weather data " +
+      JSON.stringify(data);
+    console.log(sentence);
+    // function call
+  }
+
+  // create a new function to make an API call to the OPENAI API
 
   return (
     <>
       <h1>☀️Are you dressed for the weather? ☔️ </h1>
-      <p>Tell me your location, and AI will give you suggestions for what to dress for the day </p>
+      <p>
+        Tell me your location, and AI will give you suggestions for what to
+        dress for the day{" "}
+      </p>
       <input
         type="text"
         value={input}
