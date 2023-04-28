c979e038a0f9baff8fc92d267180368a184e7170
Jack <li.jack0707@gmail.com>
2023-04-28 13:20:23 -0400

    chore: add fetch request to openai api on frontend and backend

--- weather-forecast-advisor/Client/src/App.jsx
+++ weather-forecast-advisor/Client/src/App.jsx
@@ -3,9 +3,10 @@ import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
+  const [weatherData, setWeatherData] = useState("")
 
   async function getApiKey() {
-    const response = await fetch("http://localhost:3000/api_key");
+    const response = await fetch("http://localhost:3080/api_key");
     const data = await response.json();
     const apiKey = data.apiKey;
     // use apiKey in your weather API request
@@ -17,22 +18,43 @@ function App() {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      makePrompt(data);
+      setWeatherData(data);
+      
     } catch (error) {
       console.log(error, "error fetching the api key");
     }
   }
-
   // create a function to concatenate human prompt with weather data
-  function makePrompt(data) {
-    const sentence =
-      "Could you give me some suggestions for what to wear today given the following weather data in my city, which is also inside the weather data " +
-      JSON.stringify(data);
-    console.log(sentence);
-    // function call
-  }
+  // function makePrompt(data) {
+  //   const sentence =
+  //     "Could you give me some suggestions for what to wear today given the following weather data in my city, which is also inside the weather data " +
+  //     JSON.stringify(data);
 
-  // create a new function to make an API call to the OPENAI API
+  // }
+  console.log(weatherData)
+  async function getAdvice(){
+    try{
+      const sentence =
+        "Could you give me some suggestions for what to wear today given the following weather data in my city, which is also inside the weather data " +
+        JSON.stringify(weatherData);
+
+      const response = await fetch("http://localhost:3080", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: "say this is a test",
+        }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
 
   return (
     <>
@@ -41,12 +63,14 @@ function App() {
         Tell me your location, and AI will give you suggestions for what to
         dress for the day{" "}
       </p>
+
       <input
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button onClick={() => getApiKey()}>Get Suggestion</button>
+      <button onClick={() => getApiKey()}>Get Weather Data</button>
+      {weatherData && <button onClick={() => getAdvice()}>Get Suggestions</button>}
     </>
   );
 }
--- weather-forecast-advisor/index.js
+++ weather-forecast-advisor/index.js
@@ -2,18 +2,40 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from 'express'
 import cors from 'cors'
+
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  organization: "org-PpiWTFdwhoRSdhS3xaoV71ia",
+  apiKey: process.env.OPEN_AI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration)
+
+
 const app = express();
-
-
 app.use(cors())
 
 app.get("/api_key", (req, res) => {
-  res.json({ apiKey: process.env.KEY });
+  res.json({ apiKey: process.env.OPEN_WEATHER_API_KEY});
 });
 
-app.get("/", (req, res) => {
-  res.send("All Directory");
+app.post("/", async (req, res) => {
+    const {message} = req.body
+
+    console.log(message)
+    const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: `${message}` }],
+    temperature: 0.5
+  });
+  
+  res.json({
+    message: response.data.choices[0].message.content,
+  });
+
 });
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+
+app.listen(3080, () => {
+  console.log("listening on port 3080");
 });
