67ee2060fe24d598fc4aee6ba74dc9002413e524
Jack <li.jack0707@gmail.com>
2023-04-29 16:57:00 -0400

    chore: set sidebar visible after anychange in the input

--- weather-forecast-advisor/Client/src/App.jsx
+++ weather-forecast-advisor/Client/src/App.jsx
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
+import "./App.css";
+import cityData from './cities.js'
+import SearchResults from "./components/searchResults";
+
+
 
 function App() {
   const [input, setInput] = useState("");
   const [weatherData, setWeatherData] = useState("")
   const [AiSuggestion, setAiSuggestion] = useState("")
   const [paragraphs, setParagraph] = useState("")
+  const [cityDataArr, setCityDataArr] = useState(cityData)
+  const [filteredData, setFilteredData] = useState([])
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true)
+
+
 
   async function getApiKey() {
     const response = await fetch("http://localhost:3080/api_key");
@@ -29,7 +39,7 @@ function App() {
   async function getAdvice(){
     try{
       const sentence =
-        "Could you give me a short suggestion summary and suggestions in bullet points for what to wear today given the following weather data" +
+        "Could you give me some suggestions for what to wear today given the following weather data in my city, which is also inside the weather data " +
         JSON.stringify(weatherData);
 
       const response = await fetch("http://localhost:3080", {
@@ -44,7 +54,6 @@ function App() {
 
       const data = await response.json();
       const suggestion = data.message
-      console.log(suggestion)
       setAiSuggestion(suggestion);      
     }
     catch(error){
@@ -52,8 +61,27 @@ function App() {
     }
   }
 
+  function updateSearch() {
+    if (input.length > 0) {
+      const lowercaseUserInput = input.toLowerCase();
+      const filteredDataArr = cityDataArr.filter((place) => {
+        place.lowercaseName = place.name.toLowerCase();
+        return (
+          place.lowercaseName.indexOf(lowercaseUserInput) >= 0
+        );
+      });
+      setFilteredData(filteredDataArr);
+    }
+  }
+
+  useEffect(()=>{
+    updateSearch()
+    setIsSidebarVisible(true)
+  },[input])
+
+
   return (
-    <main>
+    <>
       <h1>☀️Are you dressed for the weather? ☔️ </h1>
       <p>
         Tell me your location, and AI will give you suggestions for what to
@@ -65,6 +93,16 @@ function App() {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
+      
+      <div className="search-result-container" >
+        {isSidebarVisible &&       
+        <SearchResults 
+          setIsSidebarVisible ={setIsSidebarVisible}
+          filteredData = {filteredData}
+          setInput = {setInput}
+        /> }
+       </div>
+
       <button onClick={() => getApiKey()}>Get Weather Data</button>
       {weatherData && (
         <button onClick={() => getAdvice()}>Get Suggestions</button>
@@ -74,7 +112,7 @@ function App() {
           {AiSuggestion}
         </pre>
       )}
-    </main>
+    </>
   );
 }
 
