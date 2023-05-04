1b7c274c12910fafb0a2fb4db683348836eee89a
Jack <li.jack0707@gmail.com>
2023-05-04 17:39:13 -0400

    chore: track frontend folder

---
+++ weather-forecast-advisor/Frontend/src/App.jsx
@@ -0,0 +1,162 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import cityData from './cities.js'
+import SearchResults from "./components/searchResults";
+
+
+
+function App() {
+  const [input, setInput] = useState("");
+  const [weatherData, setWeatherData] = useState("")
+  const [AiSuggestion, setAiSuggestion] = useState("")
+
+  const [cityDataArr, setCityDataArr] = useState(cityData)
+  const [filteredData, setFilteredData] = useState([])
+  const [isCityList, setIsCityList] = useState(true)
+  const [isSearchBtnVisible, setIsSearchBtnVisible] = useState(false)
+  const [isLoadingData, setIsLoadingData] = useState(false)
+  const [suggestionContent, setSuggestionContent] = useState(
+    <h2>Ready when you are!</h2>
+  );
+
+
+  async function getApiKey() {
+    const response = await fetch("http://localhost:5050/api_key");
+    const data = await response.json();
+    const apiKey = data.apiKey;
+    // use apiKey in your weather API request
+    getWeatherData(apiKey);
+  }
+
+
+  async function getWeatherData(apiKey) {
+    setIsLoadingData(true)
+    const city = filteredData[0]
+    const lon = city.lon
+    const lat = city.lat
+  
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setWeatherData(data);
+    
+      
+    } catch (error) {
+      console.log(error, "error fetching the api key");
+    }
+  }
+
+
+  // make fetch call to open API when weatherdata exists
+  useEffect(()=>{
+    if(weatherData && filteredData[0] && input === filteredData[0].name){
+
+      const sentence =
+        "Could you give me some suggestions for what to wear today given the following weather data in my city. In your response give a two to three setence summary, actionable suggestions in bulletpoints, and convert any kelvin measurements into celsius " +
+        JSON.stringify(weatherData);
+
+      fetch("http://localhost:5050", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: sentence,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const suggestion = data.message;
+          setAiSuggestion(suggestion);
+          setIsLoadingData(false);
+        })
+        .catch((error) => {
+          setIsLoadingData(false);
+          console.log(error);
+        });
+    }
+  
+  },[weatherData])
+
+  // verifying if input exists and populating city list if no input is detected
+
+  useEffect(()=>{
+    if (input.length > 0) {
+      const lowercaseUserInput = input.toLowerCase();
+      const filteredDataArr = cityDataArr.filter((place) => {
+        place.lowercaseName = place.name.toLowerCase();
+        return place.lowercaseName.indexOf(lowercaseUserInput) >= 0;
+      });
+
+      setFilteredData(filteredDataArr);
+    }
+    if (input === "") {
+      setIsCityList(true);
+      setFilteredData(cityData);
+    }
+  }, [input])
+
+  // disable visibility of city list and search button based on state of input 
+  
+  useEffect(()=>{
+    if(filteredData.length === 1 && filteredData[0].name === input){
+      setIsCityList(false);
+      setIsSearchBtnVisible(true)
+    }
+    else{
+      setIsCityList(true);
+      setIsSearchBtnVisible(false);
+    }
+
+  },[filteredData])
+
+
+  // conditionally render text based on loading status
+  useEffect(()=>{
+    
+    if(!AiSuggestion && isLoadingData){
+      setSuggestionContent(<h2>Getting your suggestion ... </h2>)
+    }
+    else if(AiSuggestion && !isLoadingData){
+      setSuggestionContent(<pre>{AiSuggestion}</pre>);
+    }
+
+  },[isLoadingData, AiSuggestion])
+
+  return (
+    <>
+      <h1>☀️Are you dressed for the weather? ☔️ </h1>
+      <p>
+        Tell me your location, and AI will give you suggestions for what to
+        dress for the day{" "}
+      </p>
+
+      <main className="main">
+        <section className="input-container">
+          <input
+            type="search"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          <div className="inline">
+            {isSearchBtnVisible && (
+              <button onClick={() => getApiKey()}>Get Suggestion</button>
+            )}
+          </div>
+        </section>
+
+        <div className="search-result-container">
+          {isCityList && (
+            <SearchResults filteredData={filteredData} setInput={setInput} />
+          )}
+        </div>
+
+        <div className="ai-response-container">{suggestionContent}</div>
+      </main>
+    </>
+  );
+}
+
+export default App;
